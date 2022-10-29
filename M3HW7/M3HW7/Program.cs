195bef9df210e5cebffa5a69ea1074ad108f25de
Max <maksym.illarionov@nure.ua>
2022-10-29 19:01:48 +0300

    Empty project

---
+++ M3HW7/M3HW7/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace M3HW7
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
